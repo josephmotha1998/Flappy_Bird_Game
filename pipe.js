d920eb18486db3f898b11665e0425f29e6b772a1
Joseph Motha <josephmotha@outlook.com>
2019-05-14 20:02:16 -0400

    more precise pipe creation

--- pipe.js
+++ pipe.js
@@ -1,6 +1,6 @@
 function Pipe(){
 	this.top=random(height/2);
-	this.bottom=random(height/2);
+	this.bottom=this.top+80;
 	this.x=width;
 	this.w=20;
 	this.speed=2;
@@ -11,19 +11,23 @@ function Pipe(){
 			fill(255,0,0);
 		}
 		rect(this.x,0,this.w,this.top);
-		rect(this.x,height-this.bottom,this.w,this.bottom);  
+		rect(this.x,this.bottom,this.w,height-(this.bottom));  
 	}
 
 	this.highlight=false;
 
 	this.hits=function(bird){
-		if(bird.y<this.top||bird.y>height-this.bottom){
+		if(bird.y<this.top||bird.y>this.bottom){
 			if(bird.x>this.x&&bird.x<this.x+this.w){
 				this.highlight=true;
 				return true;
 			}
 		}
 		return false;
+	}
+
+	this.collision=function(){
+
 	}
 
 	this.update=function(){
